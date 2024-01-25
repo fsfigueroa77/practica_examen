fe43281bb543ae255b4d016697f3a677096fa810
fsfigueroa77 <fsfigueroa77@gmail.com>
2024-01-25 03:59:08 -0500

    Update practica.py

--- python/practica.py
+++ python/practica.py
@@ -27,8 +27,8 @@ def arg_greet(name):
 arg_greet("Felipe")
 """
 
-print("Programa para calcular el IVA")
-
+"""print("Programa para calcular el IVA")"""
+"""
 valor = float(input("Caul es el valor de la compra: "))
 
 def calculo_iva(valor):
@@ -37,3 +37,18 @@ def calculo_iva(valor):
     print(f"Precio antes de IVA: {valor} USD\nValor del IVA: {iva} USD\nTotal: {total} USD")
 
 calculo_iva(valor)
+"""
+def repetir_programa():
+    global elec, hola, mundo
+    elec = int(input("Desea repetir el programa [1] SI [2] No: "))
+    print(f"{hola + mundo}")
+
+elec = 1
+hola = 10
+mundo = 20
+
+while(elec ==1):
+    nombre = input("Ingresa tu nombre: ")
+    print(f"Tu nombre es {nombre}")
+    repetir_programa()
+
